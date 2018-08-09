1c90ac0f18aab306144784436fed6241f34518de
Utkarsh Singh <30589139+Utkarsh1999@users.noreply.github.com>
2018-08-09 11:41:22 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,85 +1,177 @@
 #include <iostream>
-# define MAX 50
+#include <iomanip>
 
 using namespace std;
 
-class QueueArray
+
+class SLLNode 
 {
-	int first,last;
-	int storage[MAX];
-
-	QueueArray()
-	{
-		first=-1;
-		last=-1;
-	}
-
-
-	//enqueue
-	void enQueue(int el)
-	{
-		if(!isFull())
+	public: 
+		int info;
+		SLLNode *next;
+	
+		SLLNode()
 		{
-			if(last == (MAX-1))
-			{
-				last=0;
-				// first = 0;
-				// storage[last]=el;
-			}
-			else
-			{
-				last++;
-				// storage[last]=el;
-			}
-			storage[last]=el;
+			info = 0;
+			next = NULL;	
 		}
-	}
+
+};
 
 
 
-
-	int deQueue()
+class SLL
+{
+    public:
+	SLLNode *head , *tail;
+	
+	SLL()
 	{
-		if(!isEmpty())
+		head = NULL;
+		tail = NULL;
+	}
+	
+	void addAtTail(int info)
+	{	
+		if(head == NULL)
 		{
-			return storage[first++];	
+			SLLNode *node1 = new SLLNode();
+			node1->info = info;
+			node1->next = NULL;
+			head = node1;
+			tail = node1;
+			
 		}
 		else{
-			//return empty queue error
-			return 0;
+			SLLNode *node2 = new SLLNode();
+			node2->info = info;
+			node2->next = NULL;
+			
+			tail -> next = node2;
+			tail = node2;
+			
+		}
+	}
+
+	void visit(SLLNode *p)
+	{
+		cout<<p->info<<endl;	
+	}
+
+	void traverse()
+	{
+		SLLNode *p = new SLLNode();
+		p = head;
+		
+		
+		while(p!= NULL)
+		{
+			
+			//visit(p);
+			cout<<p->info<<endl;			
+			p = p->next;
 		}
 	}
 
 
-	bool isEmpty()
+	bool search(int el)
 	{
-		if(last == -1)
+		SLLNode *p = head;
+		while(p != NULL)
 		{
-			return true;
+			if(p->info == el)
+			{
+				return true;			
+			}
+			p = p->next;
 		}
 		return false;
 	}
 
 
-	bool isFull()
+	void addAtHead(int info)
 	{
-		if((first == 0 && last == MAX-1) || (first == last+1))
+		
+		if(head == NULL)
 		{
-			//queue is full
-			return true;
+			SLLNode *node1 = new SLLNode();
+			node1->info = info;
+			node1->next = NULL;
+			head = node1;
+			tail = node1;
+			
 		}
-			return false;	
+		else{
+			SLLNode *tempNode = new SLLNode(); 
+			tempNode->info = info;
+			tempNode->next = NULL;
+			
+			tempNode->next = head;
+			head = tempNode;
+			
+		}
 	}
 
 
-	void display()
+	int deleteFromHead()
 	{
+		
+		if(head == NULL)
+		{
+			//linkedlist is empty
+			return 0;
+			
+		}
+		else{
+			 
+			int info = head->info;			
+			head = head->next;
+			return info;
+		}	
+	}
+
+	int deleteFromTail()
+	{
+				
+		SLLNode *p = head;
+		while(p != NULL)
+		{
+				
+			
+		}
+		return false;
+
 
 	}
 
 
+	void deleteFromPosition(int pos)
+	{
+		
+	}
 
+	//concate two different lists
+	
+	SLL concatinate(SLL list2)
+	{
+		
+	}
 
 
 
 };
+
+	int main()
+	{
+		SLL list;
+
+		
+
+		return 0;
+	}
+
+
+
+
+
+
